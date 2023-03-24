f871ce91607281ccf4acaf73b2d6c1e7a3391bc0
Budi Adiono <budiadiono@gmail.com>
2023-03-25 01:00:58 +0700

    fix: 🐛 removed unused transformPositions

--- src/constants.ts
+++ src/constants.ts
@@ -4,26 +4,6 @@ import { RobotFaceDirection } from "./enums";
 export const TABLETOP_WIDTH = 5;
 export const TABLETOP_HEIGHT = 5;
 
-/**
- * Since south/west become the most corner, let's transform it!
- */
-export const TRANSFORMED_Y_POS = transformPositions(TABLETOP_HEIGHT);
-
-/**
- * Build key map for reversed sequence of numbers.
- * @param size length of numbers to be reversed.
- * @returns reversed keyed index.
- */
-function transformPositions(size: number) {
-  return Array.from({ length: size })
-    .map((_, idx) => idx)
-    .reverse()
-    .reduce((prev, curr, index) => {
-      prev[index] = curr;
-      return prev;
-    }, {} as Record<number, number>);
-}
-
 export const INDEXED_DIRECTIONS = [
   RobotFaceDirection.SOUTH,
   RobotFaceDirection.WEST,
